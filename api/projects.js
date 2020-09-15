44324b74a5d319c1d1e5f34969f352d9432a39f4
constantingoeldel <constantin_goeldel@t-online.de>
2020-09-15 14:35:47 +0200

    add cathes

--- api/projects.js
+++ api/projects.js
@@ -5,8 +5,11 @@ const router = Router()
 router.get('/', (req, res, next) => {
   connectToDatabase(process.env.MONGODB_URI)
     .then((db) => db.collection('Projects'))
+    .catch((error) => console.log('Error finding collection Projects:', error))
     .then((collection) => collection.find({}).toArray())
-    .then((projects) => res.json(projects))
+    .catch((error) => console.log('Error finding object in collection:', error))
+    .then((tags) => res.json(tags))
+    .catch((error) => console.log('Error while responding:', error))
 })
 
 export default router
--- api/tags.js
+++ api/tags.js
@@ -5,8 +5,11 @@ const router = Router()
 router.get('/', (req, res, next) => {
   connectToDatabase(process.env.MONGODB_URI)
     .then((db) => db.collection('Tags'))
+    .catch((error) => console.log('Error finding collection Tags:', error))
     .then((collection) => collection.find({}).toArray())
+    .catch((error) => console.log('Error finding object in collection:', error))
     .then((tags) => res.json(tags))
+    .catch((error) => console.log('Error while responding:', error))
 })
 
 export default router
