256c167d9b7cfed08e308166b8e20dd8bec583f3
constantingoeldel <constantin_goeldel@t-online.de>
2020-09-16 10:08:04 +0200

    test env

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import server from './api/server'
 require('dotenv').config()
 
 const port = process.env.PORT || 4000
-server.listen(port, () => console.log(`SCAAPI listening at http://localhost:${port}`))
+server.listen(port, () =>
+  console.log(`SCAAPI listening at http://localhost:${port}`, process.env.MONGODB_URI)
+)
