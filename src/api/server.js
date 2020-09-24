c9e2ba3b25cabe36e40580769861e4004d5b770e
constantingoeldel <constantin_goeldel@t-online.de>
2020-09-24 16:47:05 +0200

    add post

--- src/api/server.js
+++ src/api/server.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 
 const server = express()
 server.use(cors())
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
 
 server.use('/api/projects', projects)
 server.use('/api/tags', tags)
